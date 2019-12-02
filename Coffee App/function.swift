d93a4fff35de813b8504aeaa2c018cdb6aa2aadc
tomsenior20 <tomsenior>
2019-12-02 10:36:43 +0000

    first push

---
+++ Coffee App/function.swift
@@ -0,0 +1,64 @@
+//
+//  function.swift
+//  Coffee App
+//
+//  Created by Code Nation on 29/11/2019.
+//  Copyright © 2019 Tom Senior. All rights reserved.
+//
+
+import Foundation
+
+var icedCoffee = 0.0
+var gingerBreadLatte = 0.0
+var hotChocolate = 0.0
+var tea = 0.0
+var muffin = 0.0
+var crisp = 0.0
+var sandwich = 0.0
+var bagel = 0.0
+
+func icedCoffeeAddition() {
+    icedCoffee += 3.00
+    print(icedCoffee)
+}
+
+func gingerBreadLatteAddition(){
+    gingerBreadLatte += 3.50
+    print(gingerBreadLatte)
+}
+
+func hotChocolateAddition(){
+    hotChocolate += 2.50
+    print(hotChocolate)
+}
+
+func teaAddition()  {
+    tea += 1.50
+    print(tea)
+}
+
+func muffinAddition()  {
+    muffin += 1.00
+    print(muffin)
+}
+
+func crispAddition()  {
+    crisp += 1.00
+    print(crisp)
+}
+
+func sandwichAddition()  {
+    sandwich += 2.00
+    print(sandwich)
+}
+
+func bagelAddition()  {
+    bagel += 3.00
+    print(bagel)
+}
+
+
+func totalPrice(){
+   
+}
+ 
